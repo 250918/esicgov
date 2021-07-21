9ab7668465d2be49438e31ebe43ecb9ed8c6cdb6
Andre Moreira <amoreira_phn@icloud.com>
2021-07-20 22:52:22 -0300

    validacao

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -34,15 +34,9 @@ class LoginController extends Controller
      */
     public function __construct()
     { 
-<<<<<<< HEAD
         $this->middleware('guest')->except('logout');
         if ($this->middleware('guest')) {
             
         }
-=======
-        if (!isEmpty($this->middleware('guest'))){
-        return null;
-        }else{}
->>>>>>> pedro
     }
 }
